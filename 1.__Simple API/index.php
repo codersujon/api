6f876a1db1307c41410d4c03211880e6fa82ec8f
MD SUJAN AHMED <csesujon155@gmail.com>
2023-10-05 06:41:35 -0800

    Simple API

--- 1.__Simple API/index.php
+++ 1.__Simple API/index.php
@@ -1,3 +1,23 @@
 <?php 
-    echo "API";
+    $con = mysqli_connect("localhost","root","","api");
+    if($con) {
+        
+        $q = "SELECT * FROM api_data";
+        $result = mysqli_query($con, $q);
+        if($result){
+            header("Content-Type: JSON");
+            $i = 1;
+            while($r = mysqli_fetch_assoc($result)){
+                $response[$i]['id'] = $r['id'];
+                $response[$i]['name'] = $r['name'];
+                $response[$i]['age'] = $r['age'];
+                $response[$i]['email'] = $r['email'];
+                $i++;
+            }
+            echo json_encode($response,JSON_PRETTY_PRINT);
+        }
+        
+    }else{
+        echo "Database connection failed";
+    }
 ?>
